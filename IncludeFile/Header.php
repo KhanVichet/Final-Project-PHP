6f9dbb52ace15ea086b86f0992606288c1e69080
Khan Vichet <khanvichet1@gamil.com>
2023-11-07 17:45:58 +0700

    first commit

---
+++ IncludeFile/Header.php
@@ -0,0 +1,19 @@
+<div class="header">
+    <div class="container">
+        <div class="nav">
+            <ul>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">WHERE TO GO</a></li>
+                <li><a href="#">HOW TO GO</a></li>
+                <li><a href="#">WHERE TO STAY CONTACT</a></li>
+                <li><a href="#">ABOUT US</a></li>
+            </ul>
+        </div>
+
+        <div class="icon-socail-emdail">
+            <i class="fa-brands fa-facebook"></i>
+            <i class="fa-brands fa-instagram"></i>
+            <i class="fa-brands fa-telegram"></i>
+        </div>
+    </div>
+</header>
